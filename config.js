aa779ee69aa4519e40bd1245649e9ed9d217ba3b
Ben Turner <bbgrabbag@gmail.com>
2017-12-28 19:08:57 -0700

    edited port

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 
 module.exports = {
-    port: process.env.PORT,
+    port: process.env.PORT || 8080,
     secret: process.env.SECRET,
     db: process.env.MONGODB_URI
 }
