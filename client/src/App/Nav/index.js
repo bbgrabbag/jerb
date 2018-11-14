6cd5afe1faa2cb3c1dd8f6b81bf4b767edea3132
Anthony Agnone <ada60@pitt.edu>
2018-11-14 15:29:21 -0700

    Reworked navigation.

--- client/src/App/Nav/index.js
+++ client/src/App/Nav/index.js
@@ -1,23 +1,86 @@
-import React from 'react';
-import "./index.css";
+import React from 'react'
+import styled from 'styled-components'
 
 //packages
-import { Link, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
+import { Link, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+const Navigation = styled.div`
+  grid-area: nav;
+  width: 60%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 2em;
+  padding-right: 1em;
+  & a {
+    color: whitesmoke;
+    margin-left: 2em;
+  }
+`
+
+const Header = styled.div`
+  background-color: rgba(127, 191, 255, 0.8);
+  width: 100%;
+  height: 8vh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1em;
+  & .brand {
+    display: flex;
+    align-items: center;
+    & h1 {
+      color: whitesmoke;
+      transform: scale(0.8);
+    }
+    & a h1 {
+      color: whitesmoke;
+      transform: scale(1);
+    }
+    & a h1 i {
+      margin-right: 0.2em;
+    }
+  }
+`
 
 function Nav(props) {
-    let { isAuthenticated } = props;
-    let style = { display: isAuthenticated ? "flex" : "none" }
-    return (
-        <nav className="nav-links" style={style}>
-            <Link to="/profile-page"><i className="fa fa-home"></i></Link>
-            <Link to="/add-post"><i className="fa fa-plus"></i></Link>
-            <Link to="/view-posts"><i className="fa fa-eye"></i>
-            </Link>
-        </nav>
-    )
+  let { isAuthenticated } = props
+  let style = { display: isAuthenticated ? 'flex' : 'none' }
+
+  return (
+    <Header>
+      <div className="brand">
+        <Link to="/">
+          <h1>
+            <i className="fa fa-briefcase" aria-hidden="true" />
+            Jerb.
+          </h1>
+        </Link>
+        <h1>Where the jerbs at</h1>
+      </div>
+      <Navigation className="nav-links" style={style}>
+        <Link to="/profile-page">
+          <i className="fa fa-home" />
+        </Link>
+        <Link to="/add-post">
+          <i className="fa fa-plus" />
+        </Link>
+        <Link to="/view-posts">
+          <i className="fa fa-eye" />
+        </Link>
+      </Navigation>
+    </Header>
+  )
 }
 
-const mapStateToProps = state => ({ isAuthenticated: state.auth.isAuthenticated });
+const mapStateToProps = state => ({ isAuthenticated: state.auth.isAuthenticated })
 
-export default withRouter(connect(mapStateToProps, {})(Nav));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {}
+  )(Nav)
+)
--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,55 +1,59 @@
-import React, { Component } from 'react';
-import "./index.css";
+import React, { Component } from 'react'
+import './index.css'
 
 //packages
-import { Switch, Route, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { verify } from "../Redux/auth";
+import { Switch, Route, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { verify } from '../Redux/auth'
 
 //components
-import Header from "./Header";
-import Footer from "./Footer";
-import Nav from "./Nav";
-import Signup from "./Signup";
-import Login from "./Login";
-import ProfilePage from "./ProfilePage";
-import PostList from "./PostList";
-import PostForm from "./PostForm";
-import LoadingPage from "./LoadingPage";
-import ProtectedRoute from "./ProtectedRoute";
+import Footer from './Footer'
+import Nav from './Nav'
+import Signup from './Signup'
+import Login from './Login'
+import ProfilePage from './ProfilePage'
+import PostList from './PostList'
+import PostForm from './PostForm'
+import LoadingPage from './LoadingPage'
+import ProtectedRoute from './ProtectedRoute'
 
 class App extends Component {
-    componentDidMount() {
-        this.props.verify();
+  componentDidMount() {
+    this.props.verify()
+  }
+  render() {
+    let { loading, isAuthenticated } = this.props
+    let style = {
+      gridTemplateRows: isAuthenticated ? `15% 85% 15% 10%` : `15% 60% 15% 10%`
     }
-    render() {
-        let { loading, isAuthenticated } = this.props;
-        let style = {
-            gridTemplateRows: isAuthenticated ? `15% 85% 15% 10%` : `15% 60% 15% 10%`
-        }
-        return (
-            <div className="app-wrapper" style={style} >
-                <Nav></Nav>
-                <div className="content-wrapper">
-                    {loading ?
-                        <LoadingPage /> :
-                        <Switch>
-                            <Route path='/signup' component={Signup} />
-                            <Route exact path='/' component={Login} />
-                            <ProtectedRoute path='/add-post' component={PostForm} />
-                            <ProtectedRoute path='/profile-page' component={ProfilePage} />
-                            <ProtectedRoute path='/view-posts' component={PostList} />
-                        </Switch>
-                    }
-                </div>
-                <Header></Header>
-                <Footer></Footer>
-            </div>
-        )
-    }
+    return (
+      <div className="app-wrapper" style={style}>
+        <Nav />
+        <div className="content-wrapper">
+          {loading ? (
+            <LoadingPage />
+          ) : (
+            <Switch>
+              <Route path="/signup" component={Signup} />
+              <Route exact path="/" component={Login} />
+              <ProtectedRoute path="/add-post" component={PostForm} />
+              <ProtectedRoute path="/profile-page" component={ProfilePage} />
+              <ProtectedRoute path="/view-posts" component={PostList} />
+            </Switch>
+          )}
+        </div>
+        <Footer />
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = (state) => {
-    return { loading: state.loading, isAuthenticated: state.auth.isAuthenticated }
+const mapStateToProps = state => {
+  return { loading: state.loading, isAuthenticated: state.auth.isAuthenticated }
 }
-export default withRouter(connect(mapStateToProps, { verify })(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { verify }
+  )(App)
+)
